0ad9f998d50eac0392070a64cd43632551d2e40c
parthpatel0517 <parthpatel0517@gmail.com>
2024-05-27 19:49:10 +0530

    27_5_mainpage

--- App/Home_page/HomePage.js
+++ App/Home_page/HomePage.js
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, ImageBackground, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function HomePage() {
   return (
-    <View>
-      <Text>HomePage</Text>
-    </View>
+    <ScrollView style={style.container}>
+      <StatusBar
+        animated={true}
+        translucent backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+      <View style={{ width: '100%', height: 720, position: 'relative' }}>
+        <ImageBackground
+          source={require('../../assets/img/pexels-godisable-jacob-226636-896293.jpg')}
+          style={{
+            width: "100%",
+            height: "100%"
+          }}
+        >
+        </ImageBackground>
+        <View style={{ width: 200 }}>
+          <Text style={{ color: 'white', fontSize: 50, position: 'absolute', bottom: 150, left: 10, fontFamily: 'Metropolis-Black' }}>Fashion Sale</Text>
+
+          <TouchableOpacity style={{
+            width:180,
+            backgroundColor: '#DB3022',
+            height:50,
+            alignItems:'center',
+            padding:14,
+            borderRadius:50,
+            position: 'absolute', 
+            bottom: 80, 
+            left: 10,
+            }}>
+            <Text style={{ color: 'white', fontSize: 18, fontFamily: 'Metropolis-Medium'}}>Check</Text>
+              
+              </TouchableOpacity>
+        </View>
+      </View>
+
+    </ScrollView>
   )
 }
+
+const style = StyleSheet.create([
+  container = {
+    flex: 1,
+
+  },
+])
